47f2596c9fe7abf10d4f1ed01d5ac97ca4a5a62b
Wafaa <91634884+WafaaTalabani1@users.noreply.github.com>
2022-05-24 10:45:56 +0200

    Delete webproject/webproject directory

--- webproject/webproject/src/main/java/at/ac/fhcampuswien/webproject/appUser/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package at.ac.fhcampuswien.webproject.appUser;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
--- webproject/webproject/src/main/java/at/ac/fhcampuswien/webproject/email/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package at.ac.fhcampuswien.webproject.email;
-
-public interface EmailSender {
-    void send(String to, String email);
-}
